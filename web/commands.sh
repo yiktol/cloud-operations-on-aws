28a82446e24d1c06178ff013e946638a36a561a9
Your Name <you@example.com>
2026-07-22 17:05:16 +0800

    Updates

--- web/commands.sh
+++ web/commands.sh
@@ -3,3 +3,5 @@ aws s3 cp web/ops_dark.html s3://training.yikyakyuk.com/ops/index.html
 aws s3 cp web/instances.json s3://training.yikyakyuk.com/ops/instances.json
 
 aws s3 sync diagrams/ s3://training.yikyakyuk.com/ops/img/ --exclude "*" --include "*.png"
+
+aws s3 sync web/img/ s3://training.yikyakyuk.com/ops/img/ --exclude "*" --include "*.png"
